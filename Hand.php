1b3a1f45efb562abad4ff4cbb50ba4e49bf33726
led-ray <151374279+led-ray@users.noreply.github.com>
2023-12-24 17:43:48 +0900

    v1.2

--- Hand.php
+++ Hand.php
@@ -27,13 +27,22 @@ class Hand
         $values = [ // 点を数値化する連想配列
             "2" => 2, "3" => 3, "4" => 4, "5" => 5,
             "6" => 6, "7" => 7, "8" => 8, "9" => 9, "10" => 10,
-            "J" => 10, "Q" => 10, "K" => 10, "A" => 1
+            "J" => 10, "Q" => 10, "K" => 10, "A" => 1 // Aは1点としておく
         ];
         
         // カードのランクを取得して点数を計算
         $points = 0;
+        $A_flag = 0;
         foreach ($this->cards as $card) {
             $points += $values[$card->getnum()];
+            if ($card->getnum()==="A") { // Aが含まれているか判定
+                $A_flag = 1;
+            }
+        }
+        if ($A_flag === 1) {
+            if ($points<=11) { // Aが含まれていて、得点が11以下の場合は10を足す
+                $points += 10;
+            }
         }
         return $points;
     }
